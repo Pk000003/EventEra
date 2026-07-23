2b0f6b9bcb3927921d86986e8ade754822605c53
Pk000003 <pratikakharkar1809@gmail.com>
2026-07-23 11:48:31 +0530

    connect frontend with production backend

--- Clients/Client/src/utils/axios.js
+++ Clients/Client/src/utils/axios.js
@@ -1,24 +1,24 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
-    "Content-Type": "application/json",
+        "Content-Type": "application/json",
     },
 });
 
 api.interceptors.request.use(
     (config) => {
-    const token = localStorage.getItem("token");
+        const token = localStorage.getItem("token");
 
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
 
-    return config;
+        return config;
     },
     (error) => {
-    return Promise.reject(error);
+        return Promise.reject(error);
     }
 );
 
