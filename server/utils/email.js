7232d44db322e995ae8db248a3589c58df64bcd4
Pk000003 <pratikakharkar1809@gmail.com>
2026-07-24 20:09:45 +0530

    Fix Gmail SMTP connection for OTP emails

--- server/utils/email.js
+++ server/utils/email.js
@@ -2,20 +2,16 @@ const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
 
 dotenv.config();
-
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    }
-});
-
-transporter.verify((error, success) => {
-    if (error) {
-        console.error("SMTP Error:", error);
-    } else {
-        console.log("SMTP Server Ready");
+    },
+    tls: {
+        rejectUnauthorized: false
     }
 });
 
